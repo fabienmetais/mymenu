9091b1253963c6b8ab980ee219b215eac81a444a
Fabien Metais <f.metais@lesnouveauxateliers.com>
2017-12-12 18:10:53 +0100

    Work on the launcher

--- launcher.js
+++ launcher.js
@@ -3,9 +3,6 @@ const Gtk = imports.gi.Gtk;
 const St = imports.gi.St;
 const Clutter = imports.gi.Clutter;
 const GMenu = imports.gi.GMenu;
-const Atk = imports.gi.Atk;
-const DND = imports.ui.dnd;
-const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
 const Shell = imports.gi.Shell;
 const appSys = Shell.AppSystem.get_default();
@@ -14,8 +11,10 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
 const AppsMenu = Me.imports.appsmenu;
+const LauncherItem = Me.imports.launcheritem;
 
 const PADDING = 30;
+const COL_SPACING = 5;
 
 var Launcher = new Lang.Class({
     Name: 'Launcher',
@@ -26,6 +25,7 @@ var Launcher = new Lang.Class({
         this._appsMenu = appsMenu;
 
         this._items = {};
+        this._dragMovedItems = [];
         this._apps = {};
 
         this.actor = new St.ScrollView({
@@ -40,7 +40,7 @@ var Launcher = new Lang.Class({
         this._iconeSize = this._settings.get_double('launcher-icon-size');
 
         this._dragBox = null;
-        this._dragBoxPosition = {};
+        this._hoverPosition = {x: null, y: null};
 
         this.actor.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC);
         this.actor._delegate = this;
@@ -78,6 +78,7 @@ var Launcher = new Lang.Class({
      */
     _addApps: function () {
         let launcherData = JSON.parse(this._settings.get_string('launcher-data'));
+
         for (let index in launcherData) {
             let appId = launcherData[index];
 
@@ -90,7 +91,7 @@ var Launcher = new Lang.Class({
                 let x = this._getXGridByIndex(xIndex);
                 let y = this._getXGridByIndex(yIndex);
 
-                this._addAppItem(this._apps[appId], x, y, xIndex, yIndex);
+                this._addItem(this._apps[appId], x, y, xIndex, yIndex);
             }
         }
     },
@@ -165,17 +166,31 @@ var Launcher = new Lang.Class({
 
         x = this._getXGrid(x);
         y = this._getYGrid(y);
-        let xIndex = x / this._getGidSize();
-        let yIndex = y / this._getGidSize();
+        let xIndex = this._getXIndex(x);
+        let yIndex = this._getYIndex(y);
+        if (this._hoverPosition.x != xIndex || this._hoverPosition.y != yIndex) {
+            if (this._dragBox != null) {
+                this._dragBox.destroy();
+                this._dragBox = null;
+            }
 
-        if (this._dragBox != null && (this._dragBoxPosition.x != xIndex || this._dragBoxPosition.y != yIndex)) {
-            this._dragBox.destroy();
-            this._dragBox = null;
-            this._dragBoxPosition = {};
+            if (!source instanceof LauncherItem.LauncherItem || source.xIndex != xIndex || source.yIndex != yIndex) {
+                this._moveHoverDragItem(xIndex, yIndex);
+                this._restoreHoverDragItem(this._hoverPosition.x, this._hoverPosition.y);
+            }
+
+            if (source instanceof LauncherItem.LauncherItem && (source.xIndex != xIndex || source.yIndex != yIndex)) {
+                let x = this._getXGridByIndex(xIndex);
+                let y = this._getYGridByIndex(yIndex);
+                source.setPosition(x, y, xIndex, yIndex);
+            }
+
+
+            this._hoverPosition = {x: xIndex, y: yIndex};
         }
 
         if (this._dragBox == null) {
-            let size = this._getGidSize();
+            let size = this._getGridSize();
             this._dragBox = new St.BoxLayout({
                 fixed_position_set : true,
                 fixed_x: x,
@@ -185,7 +200,6 @@ var Launcher = new Lang.Class({
                 width: size
             });
 
-            this._dragBoxPosition = {x: xIndex, y: yIndex};
             this._appsGrid.add_actor(this._dragBox);
         }
     },
@@ -210,15 +224,21 @@ var Launcher = new Lang.Class({
 
         x = this._getXGrid(x);
         y = this._getYGrid(y);
+        let xIndex = this._getXIndex(x);
+        let yIndex = this._getYIndex(y);
 
-        let xIndex = x / this._getGidSize();
-        let yIndex = y / this._getGidSize();
-
-        this._addAppItem(app, x, y, xIndex, yIndex);
+        if (source instanceof AppsMenu.AppItem) {
+            this._addItem(app, x, y, xIndex, yIndex);
+        } else if (source instanceof LauncherItem.LauncherItem) {
+            source.actor.show();
+            let x = this._getXGridByIndex(xIndex);
+            let y = this._getYGridByIndex(yIndex);
+            source.setPosition(x, y, xIndex, yIndex);
+        }
 
         this._dragBox.destroy();
         this._dragBox = null;
-        this._dragBoxPosition = {};
+        this._hoverPosition = {};
 
         this._saveData();
     },
@@ -233,22 +253,73 @@ var Launcher = new Lang.Class({
      * @param yIndex
      * @private
      */
-    _addAppItem: function (app, x, y, xIndex, yIndex) {
-        let item = new LauncherItem(app, this._appsMenu, this._settings);
+    _addItem: function (app, x, y, xIndex, yIndex) {
+        let item = new LauncherItem.LauncherItem(app, this._appsMenu, this._settings);
 
-        item.connect('destroy', Lang.bind(this, this._removeAppItem));
+        item.connect('delete', Lang.bind(this, this._deleteItem));
 
         this.menuManager.addMenu(item.menu);
 
         item.setPosition(x, y, xIndex, yIndex);
-        this._appsGrid.add_child(item);
-        this._items[xIndex + '-' + yIndex] = item;
+        this._appsGrid.add_child(item.actor);
+
+        if (this._items[xIndex] == undefined) {
+            this._items[xIndex] = {};
+        }
+
+        this._items[xIndex][yIndex] = item;
     },
 
-    _removeAppItem: function (item) {
+    _moveHoverDragItem: function (xIndex, yIndex) {
+        log(this._items);
+        if (this._items[xIndex] != undefined && this._items[xIndex][yIndex] != undefined) {
+            let item = this._items[xIndex][yIndex];
+            if (item) {
+                let newYIndex = yIndex + 1;
+                let x = this._getXGridByIndex(xIndex);
+                let y = this._getYGridByIndex(newYIndex);
+                item.setPosition(x, y, xIndex, newYIndex);
+                this._dragMovedItems.push({x: xIndex, y: yIndex});
 
-        log(item.xIndex + '-' + item.yIndex);
-        delete this._items[item.xIndex + '-' + item.yIndex];
+                this._moveHoverDragItem(xIndex, newYIndex);
+
+                this._items[xIndex][newYIndex] = item;
+                delete this._items[xIndex][yIndex];
+            }
+        }
+    },
+
+    _restoreHoverDragItem: function (xIndex, oldYIndex) {
+        for (let index in this._dragMovedItems) {
+            let movedItem = this._dragMovedItems[index];
+
+            let currentYIndex = oldYIndex + 1;
+
+            if (movedItem.x == xIndex && movedItem.y == oldYIndex
+                && this._items[xIndex] != undefined && this._items[xIndex][currentYIndex] != undefined) {
+                let item = this._items[xIndex][currentYIndex];
+
+                let x = this._getXGridByIndex(xIndex);
+                let y = this._getYGridByIndex(oldYIndex);
+                item.setPosition(x, y, xIndex, oldYIndex);
+
+                this._items[xIndex][oldYIndex] = item;
+                delete this._items[xIndex][currentYIndex];
+
+                this._dragMovedItems.splice(index, 1);
+
+                this._restoreHoverDragItem(xIndex, currentYIndex);
+                break;
+            }
+        }
+    },
+
+    _deleteItem: function (item) {
+        delete this._items[item.xIndex][item.yIndex];
+
+        if (this._items[item.xIndex].length == 0) {
+            delete this._items[item.xIndex];
+        }
         this._saveData();
     },
 
@@ -258,24 +329,23 @@ var Launcher = new Lang.Class({
      */
     _saveData: function () {
         let data = {};
-        for (let index in this._items) {
-            let launcherItem = this._items[index];
-            if (launcherItem) {
-                data[index] = launcherItem.app.get_id();
+        for (let xIndex in this._items) {
+            for (let yIndex in this._items[xIndex]) {
+                let launcherItem = this._items[xIndex][yIndex];
+                if (launcherItem) {
+                    data[xIndex + '-' + yIndex] = launcherItem.app.get_id();
+                }
             }
         }
 
-
         let dataString = JSON.stringify(data);
-        this._settings.set_string('launcher-data', dataString);
-        log('save data');
-        log(dataString);
+       // this._settings.set_string('launcher-data', dataString);
     },
 
     getAppFromSource: function (source) {
-        if (source instanceof AppsMenu.AppItem) {
+        if (source instanceof AppsMenu.AppItem || source instanceof LauncherItem.LauncherItem) {
             return source.app;
-        } else {
+        }else {
             return null;
         }
     },
@@ -288,28 +358,54 @@ var Launcher = new Lang.Class({
     _updateItemsIconSize: function () {
 
         let iconSize = this._getIconSize();
-        for (let i = 0; i < this._items.length; i++) {
-            this._items[i].updateIconSize(iconSize);
+
+        for (let xIndex in this._items) {
+            for (let yIndex in this._items[xIndex]) {
+                let launcherItem = this._items[xIndex][yIndex];
+                if (launcherItem) {
+                    launcherItem.updateIconSize(iconSize);
+                }
+            }
         }
     },
 
     _getXGrid: function(x) {
-        return (x - (x % this._getGidSize()));
+        let xGrid = (x - (x % this._getGridSize()))
+
+        if (xGrid != 0) {
+            xGrid = xGrid + (COL_SPACING * (xGrid / this._getGridSize()));
+        }
+        return xGrid;
     },
 
     _getYGrid: function(y) {
-        return (y - (y % this._getGidSize()));
+        let yGrid = (y - (y % this._getGridSize()));
+
+        if (yGrid != 0) {
+            yGrid = yGrid + (COL_SPACING * (yGrid / this._getGridSize()));
+        }
+
+        return yGrid;
+    },
+
+    _getXIndex: function (x) {
+        return (x / (this._getGridSize() + COL_SPACING));
+    },
+    _getYIndex: function (y) {
+
+
+        return (y / (this._getGridSize() + COL_SPACING));
     },
 
     _getXGridByIndex: function(xIndex) {
-        return xIndex * this._getGidSize();
+        return (xIndex * (this._getGridSize() + COL_SPACING));
     },
 
     _getYGridByIndex: function(yIndex) {
-        return yIndex * this._getGidSize();
+        return (yIndex * (this._getGridSize() + COL_SPACING));
     },
 
-    _getGidSize: function () {
+    _getGridSize: function () {
         return this._getIconSize() + (PADDING * 2);
     },
 
@@ -317,198 +413,6 @@ var Launcher = new Lang.Class({
         return this._iconeSize;
     }
 });
-
-// Launcher item class
-var LauncherItem = new Lang.Class({
-    Name: 'LauncherItem',
-    Extends: St.BoxLayout,
-
-    // Initialize menu item
-    _init: function (app, appsMenu, settings) {
-        this._settings = settings;
-        this._appsMenu = appsMenu;
-        this.app = app;
-
-        let iconSize = this._settings.get_double('launcher-icon-size');
-
-        let padding = PADDING;
-        this.parent({
-            style_class: 'launcher-item popup-menu-item',
-            fixed_position_set : true,
-            style: 'padding: ' + padding + 'px;',
-            reactive: true,
-            track_hover: true,
-            can_focus: true,
-            accessible_role: Atk.Role.MENU_ITEM
-        });
-
-
-        this.xIndex = null;
-        this.yIndex = null;
-
-        this._iconBin = new St.Bin();
-        this.add_child(this._iconBin);
-
-        this.updateIconSize(iconSize);
-
-        this._draggable = DND.makeDraggable(this);
-        this.isDraggableApp = true;
-        /*this._draggable.connect('drag-begin', Lang.bind(this, this._onDragBegin));
-        this._draggable.connect('drag-cancelled', Lang.bind(this, this._onDragCancelled));
-        this._draggable.connect('drag-end', Lang.bind(this, this._onDragEnd));*/
-
-        this._createMenu();
-
-        this.connect('button-press-event', Lang.bind(this, this._onButtonPressEvent));
-        this.connect('button-release-event', Lang.bind(this, this._onButtonReleaseEvent));
-        this.connect('touch-event', Lang.bind(this, this._onTouchEvent));
-        this.connect('key-press-event', Lang.bind(this, this._onKeyPressEvent));
-        this.connect('notify::hover', Lang.bind(this, this._onHoverChanged));
-        this.connect('key-focus-in', Lang.bind(this, this._onKeyFocusIn));
-        this.connect('key-focus-out', Lang.bind(this, this._onKeyFocusOut));
-        this.connect('notify::visible', Lang.bind(this, this._onVisibilityChanged));
-    },
-
-    _createMenu: function () {
-        this.menu = new PopupMenu.PopupMenu(this, 1.0, St.Side.LEFT);
-        Main.uiGroup.add_actor(this.menu.actor);
-
-        this.menu.actor.hide();
-
-       /* let section = new PopupMenu.PopupMenuSection();
-        this.menu.addMenuItem(section);
-
-        let mainBox = new St.BoxLayout({
-            vertical: true,
-            style_class: 'launcher-apps-menu-main-box'
-        });
-
-        mainBox._delegate = mainBox;
-
-        section.actor.add_actor(mainBox);*/
-
-        let deleteItem = new LauncherMenuItem('delete');//_('Delete'));
-        deleteItem.connect('activate', Lang.bind(this, this._onDeleteClick));
-        //mainBox.add_child(deleteItem.actor);
-
-        this.menu.addMenuItem(deleteItem);
-    },
-
-    _onDeleteClick: function () {
-        this.destroy();
-    },
-
-    _onVisibilityChanged: function () {
-        if (!this.visible)
-            this.menu.close();
-    },
-
-    _onButtonPressEvent: function (actor, event) {
-        // This is the CSS active state
-        this.add_style_pseudo_class ('active');
-        return Clutter.EVENT_PROPAGATE;
-    },
-
-    _onButtonReleaseEvent: function (actor, event) {
-        this.remove_style_pseudo_class ('active');
-        this.activate(event);
-        return Clutter.EVENT_STOP;
-    },
-
-    _onTouchEvent: function (actor, event) {
-        if (event.type() == Clutter.EventType.TOUCH_END) {
-            this.remove_style_pseudo_class ('active');
-            this.activate(event);
-            return Clutter.EVENT_STOP;
-        } else if (event.type() == Clutter.EventType.TOUCH_BEGIN) {
-            // This is the CSS active state
-            this.add_style_pseudo_class ('active');
-        }
-        return Clutter.EVENT_PROPAGATE;
-    },
-
-    _onKeyPressEvent: function (actor, event) {
-        let symbol = event.get_key_symbol();
-
-        if (symbol == Clutter.KEY_space || symbol == Clutter.KEY_Return) {
-            this.activate(event);
-            return Clutter.EVENT_STOP;
-        }
-        return Clutter.EVENT_PROPAGATE;
-    },
-
-    _onKeyFocusIn: function (actor) {
-        this.setActive(true);
-    },
-
-    _onKeyFocusOut: function (actor) {
-        this.setActive(false);
-    },
-
-    _onHoverChanged: function (actor) {
-        this.setActive(actor.hover);
-    },
-
-    setActive: function (active) {
-        let activeChanged = active != this.active;
-        if (activeChanged) {
-            this.active = active;
-            if (active) {
-                this.add_style_class_name('selected');
-                this.grab_key_focus();
-            } else {
-                this.remove_style_class_name('selected');
-                // Remove the CSS active state if the user press the button and
-                // while holding moves to another menu item, so we don't paint all items.
-                // The correct behaviour would be to set the new item with the CSS
-                // active state as well, but button-press-event is not trigered,
-                // so we should track it in our own, which would involve some work
-                // in the container
-                this.remove_style_pseudo_class ('active');
-            }
-            //this.emit('active-changed', active);
-        }
-
-
-    },
-
-    setPosition: function (x, y, xIndex, yIndex) {
-        this.set_x(x);
-        this.set_y(y);
-        this.xIndex = xIndex;
-        this.yIndex = yIndex;
-    },
-
-    // Update the app icon
-    updateIconSize: function(iconSize) {
-        this._iconBin.set_child(this.app.create_icon_texture(iconSize));
-    },
-    activate: function(event) {
-        let mouseButton = event.get_button();
-        if (mouseButton ==  Clutter.BUTTON_PRIMARY) {
-            this.app.open_new_window(-1);
-            this._appsMenu.button.menu.toggle();
-        } else if (mouseButton ==  Clutter.BUTTON_SECONDARY) {
-            this.menu.toggle();
-        }
-    }
-});
-
-var LauncherMenuItem = new Lang.Class({
-    Name: 'LauncherMenuItem',
-    Extends: PopupMenu.PopupMenuItem,
-
-    /*_init: function (text) {
-        this.parent(text);
-    },
-
-    activate: function(event) {
-        this.parent(event);
-    },*/
-});
-
-
-
 
 // Launcher menu manager
 var LauncherMenuManager = new Lang.Class({
---
+++ launcheritem.js
@@ -0,0 +1,241 @@
+const Lang = imports.lang;
+const PopupMenu = imports.ui.popupMenu;
+const Clutter = imports.gi.Clutter;
+const DND = imports.ui.dnd;
+const Main = imports.ui.main;
+const St = imports.gi.St;
+
+const PADDING = 30;
+
+// Launcher item class
+var LauncherItem = new Lang.Class({
+    Name: 'LauncherItem',
+    Extends: PopupMenu.PopupBaseMenuItem,
+
+    // Initialize menu item
+    _init: function (app, appsMenu, settings) {
+        this._settings = settings;
+        this._appsMenu = appsMenu;
+        this.app = app;
+
+        let iconSize = this._settings.get_double('launcher-icon-size');
+
+        let padding = PADDING;
+        this.parent({
+            style_class: 'launcher-item popup-menu-item',
+            reactive: true,
+            // track_hover: true,
+            // can_focus: true,
+            // accessible_role: Atk.Role.MENU_ITEM
+        });
+
+
+        this.actor.set_y_align(Clutter.ActorAlign.CENTER);
+        this.actor.set_x_align(Clutter.ActorAlign.CENTER);
+
+
+        this.actor.set_fixed_position_set(true);
+        this.actor.set_height(iconSize + (padding * 2));
+        this.actor.set_width(iconSize + (padding * 2));
+        //this.actor.set_style('padding: ' + padding + 'px;');
+
+        this.xIndex = null;
+        this.yIndex = null;
+
+        this._iconBin = new St.Bin({
+        });
+        this.actor.add_child(this._iconBin);
+
+        log('icone size:: ' + iconSize);
+        this.updateIconSize(iconSize);
+
+
+        this._createMenu();
+
+        this._draggable = DND.makeDraggable(this.actor);
+        this.isDraggableApp = true;
+        this._draggable.connect('drag-begin', Lang.bind(this, this._onDragBegin));
+        this._draggable.connect('drag-cancelled', Lang.bind(this, this._onDragCancelled));
+        this._draggable.connect('drag-end', Lang.bind(this, this._onDragEnd));
+
+        this.actor.connect('button-press-event', Lang.bind(this, this._onButtonPressEvent));
+        this.actor.connect('button-release-event', Lang.bind(this, this._onButtonReleaseEvent));
+        this.actor.connect('touch-event', Lang.bind(this, this._onTouchEvent));
+        this.actor.connect('key-press-event', Lang.bind(this, this._onKeyPressEvent));
+        this.actor.connect('notify::hover', Lang.bind(this, this._onHoverChanged));
+        this.actor.connect('key-focus-in', Lang.bind(this, this._onKeyFocusIn));
+        this.actor.connect('key-focus-out', Lang.bind(this, this._onKeyFocusOut));
+        this.actor.connect('notify::visible', Lang.bind(this, this._onVisibilityChanged));
+    },
+
+    _createMenu: function () {
+        this.menu = new PopupMenu.PopupMenu(this.actor, 1.0, St.Side.LEFT);
+        Main.uiGroup.add_actor(this.menu.actor);
+
+        this.menu.actor.hide();
+
+        let deleteItem = new LauncherMenuItem('delete');//_('Delete'));
+        deleteItem.connect('activate', Lang.bind(this, this._onDeleteClick));
+
+        this.menu.addMenuItem(deleteItem);
+    },
+
+    _onDragBegin: function() {
+        this.actor.hide();
+        Main.overview.beginItemDrag(this);
+    },
+
+    _onDragCancelled: function() {
+        this.actor.show();
+        Main.overview.cancelledItemDrag(this);
+    },
+
+    _onDragEnd: function() {
+        this.actor.show();
+        Main.overview.endItemDrag(this);
+    },
+
+    get_app_id: function() {
+        return this.app.get_id();
+    },
+
+    getDragActor: function() {
+        let item =  new LauncherItem(this.app, this._appsMenu, this._settings);
+        return item.actor;
+    },
+
+    getDragActorSource: function() {
+        return this.actor;
+    },
+
+    setFakeActive: function(active) {
+        if (active) {
+            //  this._button.scrollToButton(this);
+            //this.actor.add_style_pseudo_class('active');
+            this.actor.add_style_class_name('selected');
+        } else {
+            //this.actor.remove_style_pseudo_class('active');
+            this.actor.remove_style_class_name('selected');
+        }
+    },
+
+    // Grab the key focus
+    grabKeyFocus: function() {
+        this.actor.grab_key_focus();
+    },
+
+    _onDeleteClick: function () {
+        this.actor.destroy();
+        this.emit('delete');
+    },
+
+    _onVisibilityChanged: function () {
+        if (!this.actor.visible)
+            this.menu.close();
+    },
+
+    _onButtonPressEvent: function (actor, event) {
+        // This is the CSS active state
+        this.actor.add_style_pseudo_class ('active');
+        return Clutter.EVENT_PROPAGATE;
+    },
+
+    _onButtonReleaseEvent: function (actor, event) {
+        this.actor.remove_style_pseudo_class ('active');
+        this.activate(event);
+        return Clutter.EVENT_STOP;
+    },
+
+    _onTouchEvent: function (actor, event) {
+        if (event.type() == Clutter.EventType.TOUCH_END) {
+            this.actor.remove_style_pseudo_class ('active');
+            this.activate(event);
+            return Clutter.EVENT_STOP;
+        } else if (event.type() == Clutter.EventType.TOUCH_BEGIN) {
+            // This is the CSS active state
+            this.actor.add_style_pseudo_class ('active');
+        }
+        return Clutter.EVENT_PROPAGATE;
+    },
+
+    _onKeyPressEvent: function (actor, event) {
+        let symbol = event.get_key_symbol();
+
+        if (symbol == Clutter.KEY_space || symbol == Clutter.KEY_Return) {
+            this.activate(event);
+            return Clutter.EVENT_STOP;
+        }
+        return Clutter.EVENT_PROPAGATE;
+    },
+
+    _onKeyFocusIn: function (actor) {
+        this.setActive(true);
+    },
+
+    _onKeyFocusOut: function (actor) {
+        this.setActive(false);
+    },
+
+    _onHoverChanged: function (actor) {
+        this.setActive(actor.hover);
+    },
+
+    setActive: function (active) {
+        let activeChanged = active != this.active;
+        if (activeChanged) {
+            this.active = active;
+            if (active) {
+                this.actor.add_style_class_name('selected');
+                this.actor.grab_key_focus();
+            } else {
+                this.actor.remove_style_class_name('selected');
+                // Remove the CSS active state if the user press the button and
+                // while holding moves to another menu item, so we don't paint all items.
+                // The correct behaviour would be to set the new item with the CSS
+                // active state as well, but button-press-event is not trigered,
+                // so we should track it in our own, which would involve some work
+                // in the container
+                this.actor.remove_style_pseudo_class ('active');
+            }
+            //this.emit('active-changed', active);
+        }
+
+
+    },
+
+    setPosition: function (x, y, xIndex, yIndex) {
+        this.actor.set_x(x);
+        this.actor.set_y(y);
+        this.xIndex = xIndex;
+        this.yIndex = yIndex;
+    },
+
+    // Update the app icon
+    updateIconSize: function(iconSize) {
+        this._iconBin.set_child(this.app.create_icon_texture(iconSize));
+    },
+
+    activate: function(event) {
+        let mouseButton = event.get_button();
+        if (mouseButton == Clutter.BUTTON_PRIMARY) {
+            this.app.open_new_window(-1);
+            this._appsMenu.button.menu.toggle();
+        } else if (mouseButton ==  Clutter.BUTTON_SECONDARY) {
+            this.menu.toggle();
+        }
+    }
+});
+
+
+var LauncherMenuItem = new Lang.Class({
+    Name: 'LauncherMenuItem',
+    Extends: PopupMenu.PopupMenuItem,
+
+    /*_init: function (text) {
+        this.parent(text);
+    },
+
+    activate: function(event) {
+        this.parent(event);
+    },*/
+});
